e9ca31fad0563789e357abd810131fa030d16dc8
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-10 18:49:35 +0530

    data-ns-test attribute added

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,8 @@ const App = () => {
       {projectDetail.map((project) => {
       return (
         <div>
-        <h1 data-ns-test={project.name}>{project.name}</h1>
-        <h6 data-ns-test={project.description}>{project.description}</h6>
+        <h1 data-ns-test='project-name'>{project.name}</h1>
+        <h6 data-ns-test='project-description'>{project.description}</h6>
         </div>
       )
         
