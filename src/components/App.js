c96e3dbbafb851b6b1708e0f9b049a1a9905e946
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-10 18:46:07 +0530

    Date rendered dynamically

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+let projectDetail = [
+  {name: 'Project 1', description: 'This is project 1'},
+  {name: 'Project 2', description: 'This is project 2'},
+  {name: 'Project 3', description: 'This is project 3'},
+]
+
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+      {projectDetail.map((project) => {
+      return (
+        <div>
+        <h1 data-ns-test={project.name}>{project.name}</h1>
+        <h6 data-ns-test={project.description}>{project.description}</h6>
+        </div>
+      )
+        
+      })}
+    </div>
   )
 }
 
